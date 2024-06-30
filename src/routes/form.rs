d60977dace9714c8e17f2c32b5189f8ed2cdffe9
Abdo Omar <abdelrahmanorigin@gmail.com>
2024-06-30 18:58:11 +0300

    fromRequest, customExtractor example
    commented out example, and you can do "go to definition" on the `Form`
    extractor for eg. to see how it is implemented using FromRequest

--- src/routes/form.rs
+++ src/routes/form.rs
@@ -22,3 +22,45 @@ pub async fn accept_form(Form(input): Form<Input>) -> impl IntoResponse {
     };
     template
 }
+
+// // FromRequest example, aka custom extractor
+// use axum::extract::Request;
+// use axum::RequestExt;
+// use axum::response::Response;
+// use axum::{async_trait, extract::FromRequest, Form};
+// use axum::http::{header::CONTENT_TYPE, StatusCode};
+// https://github.com/tokio-rs/axum/blob/main/examples/parse-body-based-on-content-type/src/main.rs
+// #[derive(Debug)]
+// pub struct CustomForm<T>(T);
+
+// #[async_trait]
+// impl<S, T> FromRequest<S> for CustomForm<T>
+// where
+//     S: Send + Sync,
+//     // Json<T>: FromRequest<()>,
+//     Form<T>: FromRequest<()>,
+//     T: 'static,
+// {
+//     type Rejection = Response;
+
+//     async fn from_request(req: Request, _state: &S) -> Result<Self, Self::Rejection> {
+//         let content_type_header = req.headers().get(CONTENT_TYPE);
+//         let content_type = content_type_header.and_then(|value| value.to_str().ok());
+
+//         if let Some(content_type) = content_type {
+//             // if content_type.starts_with("application/json") {
+//             //     let Json(payload) = req.extract().await.map_err(IntoResponse::into_response)?;
+//             //     return Ok(Self(payload));
+//             // }
+
+//             if content_type.starts_with("application/x-www-form-urlencoded") {
+//                 let Form(payload) = req.extract().await.map_err(IntoResponse::into_response)?;
+//                 return Ok(Self(payload));
+//             }
+//         }
+
+//         Err(StatusCode::UNSUPPORTED_MEDIA_TYPE.into_response())
+//     }
+// }
+// CustomExtractorError
+// https://github.com/tokio-rs/axum/blob/main/examples/customize-extractor-error/src/custom_extractor.rs
